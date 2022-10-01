aa7eeb79c48e0b3a1264c1cd96b79eba21d9ec5d
WYousefSaid <wyousefsaid@proton.me>
2022-10-01 21:24:11 +0200

    operate function and display num on press

--- logic.js
+++ logic.js
@@ -1,12 +1,37 @@
-const operators = {
-    "+": (a, b) => a + b,
-    "-": (a, b) => a + b,
-    "*": (a, b) => a + b,
-    "/": (a, b) => a + b,
+const buttons = document.querySelectorAll('.number');
+const screen = document.querySelector('.screen');
+for (let i = 0; i<buttons.length;i++){
+    buttons[i].addEventListener("click",() =>{
+        let displayValue = screen.textContent = buttons[i].textContent;
+    });
 }
 
-const operate = ((num1, operator,num2) => {
-   if (operator in operators){
-       return operators[operator](num1, num2);
-   }
-});
+
+// <== functions for basic math start ==>
+const addition = (a, b) => {
+    return a + b
+};
+const subtraction = (a, b) => {
+    return a - b
+};
+const division = (a, b) => {
+    return a / b
+};
+const multiply = (a, b) => {
+    return a * b
+};
+
+// <== functions for basic math end ==>
+
+function operate(a, oper, b) {
+    if (oper === "+") {
+        return addition(20, 30);
+    } else if (oper === "-") {
+        return subtraction(20, 30)
+    } else if (oper === "/") {
+        return division(20, 30)
+    } else if (oper === "*") {
+        return multiply(20, 30)
+    }
+}
+
